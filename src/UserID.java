7b0e1a6497fb35a9fe6d6c8266ab7a704f1fa8c9
marclmao <119444616+marclmao@users.noreply.github.com>
2023-11-21 09:19:05 -0500

    UserID added

--- src/UserID.java
+++ src/UserID.java
@@ -3,16 +3,15 @@
 // kongjika
 
 import java.util.Scanner;
-public class Main {
+public class UserID {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("enter ur name: ");
         String name = scan.nextLine();
         int space = (name.indexOf(" "));
         int length = (name.length());
+        System.out.println(name.substring(space, length) + name.charAt(0));
         System.out.println(name.substring(0, space));
-        System.out.println(name.substring(space, length));
-
 
     }
 }
